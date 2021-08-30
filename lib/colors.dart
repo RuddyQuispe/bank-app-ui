a9977f1eaa1faca4d7ce752b71ca2da1d6fda5d6
Ruddy Quispe <ruddyq18@gmail.com>
2021-08-30 01:24:30 -0400

    build: assigned colors ui

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+// this class contain all the color in app
+class AppColors {
+  static const BACK_GROUND_COLOR = Color(0xFFE9EBFF);
+  static const BUTTON_COLOR = Color(0xFF20256B);
+  static const MAIN_COLOR = Color(0xFF20256B);
+}
